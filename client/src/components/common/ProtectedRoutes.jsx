ae2efb503c31adce0d6dad2b0f267c9a8ca93036
Akriti singh <singhakriti617@gmail.com>
2026-04-24 19:07:22 +0530

    Fix: Update API URL for Render deployment and fix ProtectedRoutes loading state

--- client/src/components/common/ProtectedRoutes.jsx
+++ client/src/components/common/ProtectedRoutes.jsx
@@ -1,7 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
+import Loader from "./Loader";
 
 export default function ProtectedRoutes() {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
+  
+  if (loading) {
+    return <Loader />;
+  }
+  
   return user ? <Outlet /> : <Navigate to="/login" replace />;
 }
